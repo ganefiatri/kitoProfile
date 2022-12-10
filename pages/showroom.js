b2114685842800934304b6c3bccf90c79da02241
unknown <gicerf@gmail.com>
2022-12-11 00:06:41 +0700

    fixed prisma multiple times

--- pages/showroom.js
+++ pages/showroom.js
@@ -1,4 +1,4 @@
-import { prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import Head from 'next/head';
 import React from 'react';
 import Banner from '../components/about/Banner';
