3a681dc7a7c3f6258ab591adbfbf3c34646e6c3b
unknown <gicerf@gmail.com>
2023-01-19 15:02:11 +0700

    fixed issues

--- pages/api/product/editdata.js
+++ pages/api/product/editdata.js
@@ -121,7 +121,7 @@ export default async (req, res) => {
                     }
 
                 } catch (error) {
-                    console.log(error)
+                    return res.status(500).json({error: "Failed to send data!"})
                 }
             }
 
